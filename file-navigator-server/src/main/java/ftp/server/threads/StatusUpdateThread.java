043c63bd9c36a5a1a948b03ee65aaf3f6f89ccbd
Jordan Hall <jordanhall1995@hotmail.co.uk>
2020-12-09 00:15:22 +0000

    Moved code to the correct class

--- file-navigator-server/src/main/java/ftp/server/threads/StatusUpdateThread.java
+++ file-navigator-server/src/main/java/ftp/server/threads/StatusUpdateThread.java
@@ -1,4 +1,20 @@
 package ftp.server.threads;
 
-public class StatusUpdateThread {
+import ftp.server.objects.ClientInteractions;
+
+public class StatusUpdateThread extends Thread {
+	
+	@Override
+	public void run() {
+		
+		while (true) {
+			System.out.println("Operations: " + ClientInteractions.getNumberOfOperations() + " Clients: "
+							+ ClientInteractions.getNumberOfClientsConnected());
+			try {
+				Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- file-navigator-server/src/test/java/ftp/server/threads/StatusUpdateThreadTest.java
+++ file-navigator-server/src/test/java/ftp/server/threads/StatusUpdateThreadTest.java
@@ -3,17 +3,6 @@ package ftp.server.threads;
 
 import ftp.server.objects.ClientInteractions;
 
-public class StatusUpdateThreadTest extends Thread {
+public class StatusUpdateThreadTest {
 
-  public void run() {
-    while (true) {
-      System.out.println("Operations: " + ClientInteractions.getNumberOfOperations() + " Clients: "
-          + ClientInteractions.getNumberOfClientsConnected());
-      try {
-        Thread.sleep(30000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 }
